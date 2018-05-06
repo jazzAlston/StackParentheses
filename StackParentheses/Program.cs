46b5972423591566d50ec93fbea6e1e9433aa386
jazz8603 <jazz8603@gmail.com>
2018-05-06 10:19:17 -0700

    Add project files.

---
+++ StackParentheses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isValidString("{}(((())))")); 
+        }
+        public static bool isValidString(string str)
+        {
+            Stack<char>stack = new Stack<char>();
+            foreach (char c in str)
+            {
+                    if(c != ')' && c != '}' && c != ']')
+                    {
+                        stack.Push(c);
+                        continue;
+                    }
+                char current = stack.Peek();
+                if (current == 40 && c == 41 || current == 90 && c == 93 || current == 123 && c == 125)
+                {
+                    stack.Pop();
+
+                }
+
+                     
+            }
+            return stack.Count == 0 ? true : false;
+            
+        }
+    }
+}
